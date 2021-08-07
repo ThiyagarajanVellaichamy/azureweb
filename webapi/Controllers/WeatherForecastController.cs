49f7c947e05435b84f248f2ee55c98e7c93264a4
ThiyagarajanVellaichamy <63137672+ThiyagarajanVellaichamy@users.noreply.github.com>
2021-08-08 00:31:00 +0530

    asdf

--- webapi/Controllers/WeatherForecastController.cs
+++ webapi/Controllers/WeatherForecastController.cs
@@ -26,8 +26,9 @@ namespace webapi.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            this._logger.LogInformation("nothing");
             var rng = new Random();
-            return Enumerable.Range(1, 2).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
